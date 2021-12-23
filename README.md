d1cd20289b418e5f19eb862a0a04b8d62d532cdb
Ak-ko <Ak-ko@users.noreply.github.com>
2021-12-23 19:22:31 +0000

    automated commit by submit50

---
+++ README.md
@@ -0,0 +1,14 @@
+# MLBB Diamond Shop
+#### Video Demo:  <URL HERE>
+#### Description:
+This is diamond selling page but still incomplete in somepoint. I hope you guys enjoy it.
+
+First thing is you need to login with your userid and zone id corresponding with your mlbb. If you don't play mlbb just put 111111111 and 1111 in it and it will
+work.
+
+Next, you are now in home page and you can buy some diamonds with the project gave as gift which is 500 coins and if you wish, you are able to add more.
+Those coins can be bought by your bankaccount which is artifical bank account and I put 100000 no unit money in your bank.
+
+If you write least character of userid and zoneid, you will found some problem and you will see the Not Found page as the result.
+
+Thanks for watching and using. I will try my best.
---
+++ application.py
@@ -0,0 +1,207 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, jsonify, make_response
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from assistance import login_required
+
+
+
+app = Flask(__name__)
+
+# initialization the database
+db = SQL('sqlite:///diamond.db')
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+COINAMOUNT = [100,250,400,550,1000,2500]
+COINPRICE = [100,250,400,550,1000,2500]
+
+discount_give = 5
+DISCOUNT = (( discount_give / 100) * 100 )
+
+DIAPRICE = [50,150,300,450,700,1200,2000,3100,4800]
+DIAAMOUNT = [40,120,201,423,808,1150,1900,2900,3900]
+
+# for cache
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+### home ###
+@app.route("/")
+@login_required
+def index():
+
+    users = session["user_id"]
+
+    # getting the userid
+    userids = db.execute('SELECT userid FROM userinfos WHERE id = ?', users)
+    userid = userids[0]["userid"]
+
+    # getting the zoneid
+    zoneids = db.execute('SELECT zoneid FROM userinfos WHERE userid = ?', userid)
+    zoneid = zoneids[0]["zoneid"]
+
+    # coin amount
+    coins = db.execute('SELECT coinamount FROM userinfos WHERE userid = ?', userid)
+    coin_auto = coins[0]["coinamount"]
+
+    coinbought = request.form.get('coinamount')
+
+    # getting the prices
+    coins = db.execute('SELECT * FROM coins')
+    diamonds = db.execute('SELECT * FROM diamonds')
+
+    diamondjson = [
+        {"amount":40, "price" : 50},
+        {"amount":120, "price" : 150},
+        {"amount":201, "price" : 300},
+        {"amount":423, "price" : 450},
+        {"amount":808, "price" : 700},
+        {"amount":1150, "price" : 1200},
+        {"amount":1900, "price" : 2000},
+        {"amount":2900, "price" : 3100},
+        {"amount":3900, "price" : 4800},
+    ]
+
+    # Select Diamonds from db
+    diabuy = db.execute("SELECT diaamount FROM diamondbuy WHERE diabuyerid = ?", session["user_id"])
+
+
+
+    coinjson = [
+        {"amount" : 100, "price" : 100},
+        {"amount" : 250, "price" : 250},
+        {"amount" : 400, "price" : 400},
+        {"amount" : 550, "price" : 550},
+        {"amount" : 1000, "price" : 1000},
+        {"amount" : 2500, "price" : 2500},
+    ]
+
+    usermoney = db.execute("SELECT bankmoney FROM userinfos WHERE id = ?", session["user_id"])
+
+    bank = usermoney[0]["bankmoney"]
+
+    bankmoney = [
+        {
+            "amount" : bank
+        }
+    ]
+
+
+    # putting the coins into the database
+    coinamount = 0
+    coinprice = 0
+    while coinamount < len(COINAMOUNT):
+        while coinprice < len(COINPRICE):
+            if len(coins) == 0:
+                db.execute('INSERT INTO coins(coinamount,coinprice) VALUES (?,?)', COINAMOUNT[coinamount], COINPRICE[coinprice])
+            coinprice += 1
+            coinamount += 1
+        coinamount = len(COINAMOUNT)
+
+    # putting the diamonds into the database
+    diaamount = 0
+    diaprice = 0
+    while diaamount < len(DIAAMOUNT):
+        while diaprice < len(DIAPRICE):
+            if len(diamonds) == 0:
+                db.execute('INSERT INTO diamonds(diamondamount,diamondprice) VALUES (?,?)', DIAAMOUNT[diaamount], DIAPRICE[diaprice])
+            diaprice += 1
+            diaamount += 1
+        diaamount = len(DIAAMOUNT)
+
+    return render_template(
+            'index.html',
+
+            userid=userid,
+
+            zoneid=zoneid,
+
+            coin=coin_auto,
+
+            coin_bought=coinbought,
+
+            coinprices=COINPRICE,
+
+            coinjson = coinjson,
+
+            diamonds=DIAAMOUNT,
+
+            diamondbuy=diabuy,
+
+            diamondjson=diamondjson,
+
+            bankmoney=bankmoney,
+
+            discount=DISCOUNT,
+        )
+
+
+### login ###
+@app.route('/login',methods=["GET", "POST"])
+def login():
+    # forget any previous user
+    session.clear()
+
+    if request.method == "POST":
+
+        # get the details of a user
+
+        if len(request.form.get("userid")) is not 9:
+            return render_template('error.html',status = "INVALID USERID",code = 400, message = "Sir, please provide valid your userid.")
+
+        if len(request.form.get("zoneid")) is not 4:
+            return render_template('error.html',status="INVALID ZONEID",code=400,message="Sir, please provide valid your zoneid")
+
+        rows = db.execute("SELECT * FROM userinfos WHERE userid = ?", request.form.get('userid'))
+
+
+        if len(rows) != 1:
+            ids = db.execute('INSERT INTO userinfos (userid,zoneid) VALUES (?,?)', request.form.get('userid'), request.form.get('zoneid'))
+
+            #getting the user details again
+            rows = db.execute("SELECT * FROM userinfos WHERE userid = ?", request.form.get('userid'))
+
+            session["user_id"] = rows[0]["id"]
+            return redirect('/')
+        else:
+            session["user_id"] = rows[0]["id"]
+            return redirect('/')
+
+        return redirect('/')
+    else:
+        return render_template("login.html")
+
+### logout ###
+@app.route("/logout")
+def logout():
+
+    #forget any users
+    session.clear()
+
+    return redirect('/')
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return e.name,e.code
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
+
